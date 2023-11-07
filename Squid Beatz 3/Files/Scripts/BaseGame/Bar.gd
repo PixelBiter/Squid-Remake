eb50f85e89ed9c5deb1b26d5390d138c705b229a
PixelToad <109037405+PixelBiter@users.noreply.github.com>
2023-11-07 21:45:39 +0000

    Removes Old Worse Version

--- Squid Beatz 3/Files/Scripts/BaseGame/Bar.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-
-func _ready():
-	if G.CurrentSongBeats[G.SongSection] == 1:
-		texture = load("res://Files/Sprites/SpriteSetA/Notes/BarBigBeat.png")
-	if G.CurrentSongBeats[G.SongSection] == 2:
-		texture = load("res://Files/Sprites/SpriteSetA/Notes/BarSmallBeat.png")
-		scale = Vector2(1,1)
